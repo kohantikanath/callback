1d2588cbd6e4976c90d591b00503e44bff07058b
kohantikanath <kohan.nath@gmail.com>
2024-06-21 11:16:39 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useCallback, useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [count, setCount] = useState(0);
+  const incrementCount = useCallback(() => setCount(count + 1), [count]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type='text'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></input>
+      <button onClick={incrementCount}>Increment Counter</button>
+      <h3>Input text: {input}</h3>
+      <h3>count: {count}</h3>
+      <hr/>
+      <ChildComponent count={count} incre={incrementCount}/>
+    </div>
+  );
+}
+
+const ChildComponent = ({count, incre}) => {
+  return (
+    <div>
+      <h2>This is a child component</h2>
+      <h4>count: {count}</h4>
+      <button onClick={incre}>Increment Counter</button>
     </div>
   );
 }
